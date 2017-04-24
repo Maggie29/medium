321a59f1d88da64d167c7695002b18e0b11d2ad0
Nicolas Rasmussen <Nicolas Rasmussen>
2017-04-24 13:14:25 -0500

    section

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,19 @@
 window.addEventListener("load", function(event) {
   event.preventDefault();
   var header = document.createElement("header");
+  var divContainer = document.createElement("nav")
   var divImage = document.createElement("div");
   var divIcon = document.createElement("div");
   var divSign = document.createElement("div");
+  divContainer.className = "container"
   divImage.className = "image";
   divIcon.className = "icon";
   divSign.className = "sign";
   document.body.appendChild(header);
-  header.appendChild(divImage);
-  header.appendChild(divIcon);
-  header.appendChild(divSign);
+  header.appendChild(divContainer);
+  divContainer.appendChild(divImage)
+  divContainer.appendChild(divIcon);
+  divContainer.appendChild(divSign);
 
   var imgMedium = document.createElement("img");
   imgMedium.width = "100";
@@ -36,4 +39,21 @@ window.addEventListener("load", function(event) {
   divIcon.appendChild(divFacebook);
   divFacebook.appendChild(spanFacebook);
 
+  var spanSign = document.createElement("span");
+  spanSign.innerText = "Sign in / Sign up";
+  divSign.appendChild(spanSign);
+
+  var section = document.createElement("section")
+  var divWriter = document.createElement("div");
+  var news = document.createElement("article");
+  divWriter.className = "div-writer";
+
+/*  var imgWriter = document.createElement("img");
+  imgWriter.width = "70";
+  imgWriter.src = "assets/img/belen.jpg";*/
+
+document.body.appendChild(section);
+section.appendChild(divWriter);
+//divWriter.appendChild(imgWriter);
+section.appendChild(news);
 });
